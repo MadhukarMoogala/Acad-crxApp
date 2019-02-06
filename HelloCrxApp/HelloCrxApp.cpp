b553c538f8ea5eceb67f144fd251bec548b73a28
Madhukar Moogala <madhukar.moogala@autodesk.com>
2019-02-06 12:09:08 +0530

    FirstCommit

---
+++ HelloCrxApp/HelloCrxApp.cpp
@@ -0,0 +1,57 @@
+// HelloCrxApp.cpp : Defines the exported functions for the DLL application.
+//
+
+// TestCrx.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include <aced.h> 
+#include <rxregsvc.h> 
+
+void initApp();
+void unloadApp();
+
+void helloWorld();
+
+void initApp()
+{
+	// register a command with the AutoCAD command mechanism 
+	acedRegCmds->addCommand(_T("HELLOWORLD_COMMANDS"),
+		_T("Hello"),
+		_T("Bonjour"),
+		ACRX_CMD_TRANSPARENT,
+		helloWorld);
+}
+
+void unloadApp()
+{
+	acedRegCmds->removeGroup(_T("HELLOWORLD_COMMANDS"));
+}
+
+void helloWorld()
+{
+	acutPrintf(_T("\nHello World!"));
+}
+
+extern "C" AcRx::AppRetCode acrxEntryPoint(AcRx::AppMsgCode msg, void* pkt)
+{
+	switch (msg)
+	{
+	case AcRx::kInitAppMsg:
+		acrxDynamicLinker->unlockApplication(pkt);
+		acrxRegisterAppMDIAware(pkt);
+		initApp();
+		break;
+	case AcRx::kUnloadAppMsg:
+		unloadApp();
+		break;
+	default:
+		break;
+	}
+	return AcRx::kRetOK;
+}
+
+
+
+
+
